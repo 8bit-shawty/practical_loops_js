d1f7d24496177f6a9d3204381c48ef5c9b099cb6
MF <mfregwork21@gmail.com>
2024-09-15 19:03:12 -0700

    Part1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+//PART 1
+
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
+for(i = 1; i <= 100; i++){
+    if(i % 3 ===0 && i % 5 === 0){
+        console.log(i, " FIZZ BUZZ")
+    }else if(i % 3 === 0){
+        console.log(i, " FIZZ")
+    } else if(i % 5 === 0){
+        console.log(i, " BUZZ")
+    }else{
+        console.log(i)
+    }
+}
+//PART 2
